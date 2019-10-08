330df6e9823485cfcccf87934f177ebf16707b37
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-10-08 14:32:17 -0400

    formspartial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,157 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-    return (
-        <div>
-            <p>React still here!</p>
-        </div>
-    )
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            isLoginOpen: true,
+            isRegisterOpen: false
+        }
+    };
+
+    showLoginBox(){
+        this.setState({isLoginOpen: true, isRegisterOpen: false})
+    }
+    showRegisterBox(){
+        this.setState({isRegisterOpen: true, isLoginOpen: false})
+    }
+
+    render() {
+
+        return (
+            <div className="root-container">
+
+
+                <div className="box-controller">
+                    <div className={"controller "+(this.state.isLoginOpen ? "selected-controller":"")} onClick={this.showLoginBox.bind(this)}>Login</div>
+                    <div className={"controller "+(this.state.isRegisterOpen ? "selected-controller":"")} onClick={this.showRegisterBox.bind(this)}>Register</div>
+
+                </div>
+
+
+                {this.state.isLoginOpen && <LoginBox/>}
+                {this.state.isRegisterOpen && <RegisterBox/>}
+            </div>
+        )
+    }
+}
+
+class LoginBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    submitLogin(e) {}
+
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">
+                    Login
+                </div>
+                <div className="box">
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" className="login-input" placeholder="Username"/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" className="login-input" placeholder="Password"/>
+                    </div>
+                    <button type="button" className="login-btn" onClick={this.submitLogin.bind(this)}>Login</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class RegisterBox extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    showValidationErr(elm, msg){
+        this.setState((prevState)=>({
+            errors: [
+                ...prevState.errors, {
+                    elm,
+                    msg
+                }
+            ]
+        }));
+    }
+
+    clearValidationErr(elm){
+        this.setState((prevState) => {
+            let newArr = [];
+            for (let err of prevState.errors){
+                if (elm != err.elm){
+                    newArr.push(err);
+                }
+            }
+            return {errors: newArr};
+        });
+    }
+
+    onUsernameChange(e){
+        this.setState({username: e.target.value});
+        this.clearValidationErr("username");
+    }
+
+    onEmailChange(e){
+        this.setState({email: e.target.value});
+        this.clearValidationErr("email");
+    }
+
+    onPasswordChange(e){
+        this.setState({password: e.target.value});
+        this.clearValidationErr("password");
+    }
+
+    submitRegister(e) {
+        if (this.state.username == "") {
+            this.showValidationErr("username", "Username Cannot be empty!");
+          }
+          if (this.state.email == "") {
+            this.showValidationErr("email", "Email Cannot be empty!");
+          }
+          if (this.state.password == "") {
+            this.showValidationErr("password", "Password Cannot be empty!");
+          }
+    }
+
+    render() {
+
+        return (
+            <div className="inner-container">
+                <div className="header">
+                    Register
+                </div>
+                <div className="box">
+                    <div className="input-group">
+                       <label htmlFor="username">Username</label>
+                       <input type="text" name="username" className="login-input" placeholder="Username" onChange={this.onUsernameChange.bind(this)}/>
+                    </div>
+                    <div className="input-group">
+                       <label htmlFor="email">Email</label>
+                       <input type="text" name="email" className="login-input" placeholder="Email" onChange={this.onEmailChange.bind(this)}/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" className="login-input" placeholder="Password" onChange={this.onPasswordChange.bind(this)}/>
+                    </div>
+                    <button type="button" className="login-btn" onClick={this.submitRegister.bind(this)}>Register</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
 
 ReactDOM.render(<App />, document.getElementById("app"))
+
+
+
